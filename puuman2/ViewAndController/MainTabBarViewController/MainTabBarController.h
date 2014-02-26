99a55fcf200679b7568397db5e2f09bbabf62808
Ra.（祁文龙） <AF_Ra@AFgroupdeMac-mini-2.local>
2014-02-26 17:59:19 +0800

    基本页面

---
+++ puuman2/ViewAndController/MainTabBarViewController/MainTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  MainTabBarController.h
+//  puuman2
+//
+//  Created by Ra.（祁文龙） on 14-2-26.
+//  Copyright (c) 2014年 AFITC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTabBarController : UITabBarController<UITabBarControllerDelegate>
+
+@end
