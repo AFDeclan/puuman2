2bfeba98b8c259c9beb9cc70757634ef49b432c8
Ra.（祁文龙） <AF_Ra@AFgroupdeMac-mini-2.local>
2014-06-27 17:03:56 +0800

    babyInfoFrame

---
+++ puuman2/ViewAndController/MainTabBar/BabyInfo/BabyInfoBornView.h
@@ -0,0 +1,13 @@
+//
+//  BabyInfoBornView.h
+//  puuman2
+//
+//  Created by Ra.（祁文龙） on 14-6-27.
+//  Copyright (c) 2014年 AFITC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BabyInfoBornView : UIView
+
+@end
---
+++ puuman2/ViewAndController/MainTabBar/support/CAKeyframeAnimation+DragAnimation.m
@@ -0,0 +1,27 @@
+//
+//  CAKeyframeAnimation+DragAnimation.m
+//  PuumanForPhone
+//
+//  Created by Ra.（祁文龙） on 14-6-5.
+//  Copyright (c) 2014年 Declan. All rights reserved.
+//
+
+#import "CAKeyframeAnimation+DragAnimation.h"
+
+@implementation CAKeyframeAnimation (DragAnimation)
++ (void)dragAnimationWithView:(UIView *)view andDargPoint:(CGPoint)pos
+{
+    CAKeyframeAnimation *positionAnimation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
+    //    positionAnimation.fillMode = kCAFillModeForwards;
+    //    positionAnimation.removedOnCompletion =NO;
+    positionAnimation.duration = 1;
+    CGMutablePathRef positionPath = CGPathCreateMutable();
+    positionAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseIn];
+    [positionAnimation setBeginTime:0];
+    CGPathMoveToPoint(positionPath, NULL, [view layer].position.x, [view layer].position.y);
+    CGPathAddQuadCurveToPoint(positionPath, NULL, [view layer].position.x, [view layer].position.y, [view layer].position.x +pos.x,[view layer].position.y+pos.y);
+    positionAnimation.path = positionPath;
+    [view.layer addAnimation:positionAnimation forKey:@"position"];
+}
+
+@end
